a7e5b3a41e2c9e74acd4cb8f5f15afb513ff80be
RostyslavKr <106476898+RostyslavKr@users.noreply.github.com>
2023-06-11 17:41:22 +0300

    change basename

--- src/index.js
+++ src/index.js
@@ -6,11 +6,7 @@ import App from './components/App';
 // dIfpQU87tgK6BSlC
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-<<<<<<< HEAD
   <BrowserRouter basename={process.env.PUBLIC_URL}>
-=======
-  <BrowserRouter basename="delivery-app-frontend-beryl">
->>>>>>> 78306e0a0d5779449d4112386e91b28f5a0bde9d
     <App />
   </BrowserRouter>
 );
